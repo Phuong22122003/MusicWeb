f53acf88e37b955b6b8f06bfc3f34616b0f7cd1e
nhoczui3110 <tranvietquang3110@gmail.com>
2025-03-08 01:31:59 +0700

    update header and add assets folder

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,20 +3,15 @@ import { CommonModule } from '@angular/common';
 import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { AuthInterceptor } from './interceptors/auth.interceptor';
 
-
-
 @NgModule({
   declarations: [],
-  imports: [
-    CommonModule
-  ],
-  providers:[
+  imports: [CommonModule],
+  providers: [
     {
       provide: HTTP_INTERCEPTORS,
       useClass: AuthInterceptor,
-      multi:true
-    }
-
-  ]
+      multi: true,
+    },
+  ],
 })
-export class CoreModule { }
+export class CoreModule {}
