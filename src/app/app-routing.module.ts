57888ccc5f53dd699c06819fc7e050d1bb0bceb0
Phuong22122003 <hnguyenphuong09@gmail.com>
2025-04-05 18:02:24 +0700

    layout init

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,25 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { HomepageLogoutAndroidComponent } from './modules/homepage-signed-out/components/homepage-signed-out/homepage-logout-android/homepage-logout-android.component';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () =>
-      import('./modules/visitor-home/visitor-home.module').then(
-        (m) => m.VisitorHomeModule
-      ),
+    path:"",
+    loadChildren:()=>import("./layouts/main/main.module").then((m)=>m.MainModule)
   },
   {
-    path:"discover",
-    loadChildren:()=>import("./modules/home/home.module").then((m)=>m.HomeModule)
+    path:"error",
+    loadChildren:()=>import("./layouts/errors/errors.module").then((m)=>m.ErrorsModule)
   },
-  {
-    path:"logout",
-    loadChildren:()=>import("./modules/homepage-signed-out/homepage-signed-out.module").then((m)=>m.HomepageSignedOutModule)
-  },
-  { path: 'logout/android', component: HomepageLogoutAndroidComponent },
-  { path: '**', redirectTo: 'logout', pathMatch: 'full' },
+
+  { path: '**', redirectTo: 'error/404', pathMatch: 'full' },
 
 ];
 
